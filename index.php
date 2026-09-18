2beabd8b7a92fb22614511606528ca2171fca352
Said <mansurkazbek8@gmail.com>
2026-09-18 16:20:31 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+  include 'inc/cookie.inc.php';
+
+  define('PATH_LOG', 'path.log');
+  include 'inc/log.inc.php';
+  include 'inc/headers.inc.php';
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>
+    <?=$title?>
+  </title>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" type="text/css" href="inc/style.css" />
+</head>
+
+<body>
+
+  <div id="header">
+    <!-- Верхняя часть страницы -->
+    <img src="logo.gif" width="187" height="29" alt="Наш логотип" class="logo" />
+    <span class="slogan">обо всём сразу</span>
+    <!-- Верхняя часть страницы -->
+  </div>
+
+  <div id="content">
+    <!-- Заголовок -->
+    <h1><?= $header?></h1>
+    <?php if (!$hasVisitCookie): ?>
+      <p>Спасибо, что зашли на огонек</p>
+    <?php else: ?>
+      <p>
+        Вы зашли к нам <?= htmlspecialchars((string) $visitCounter, ENT_QUOTES, 'UTF-8') ?> раз<br />
+        Последнее посещение: <?= htmlspecialchars($lastVisit, ENT_QUOTES, 'UTF-8') ?>
+      </p>
+    <?php endif; ?>
+    <!-- Заголовок -->
+    <!-- Область основного контента -->
+    <?php 
+      include 'inc/routing.inc.php'; 
+    ?>
+    <!-- Область основного контента -->
+  </div>
+  <div id="nav">
+    <!-- Навигация -->
+    <h2>Навигация по сайту</h2>
+    <ul>
+      <li><a href='index.php'>Домой</a>
+      </li>
+      <li><a href='index.php?id=contact'>Контакты</a>
+      </li>
+      <li><a href='index.php?id=about'>О нас</a>
+      </li>
+      <li><a href='index.php?id=info'>Информация</a>
+      </li>
+      <li><a href='index.php?id=gbook'>Гостевая книга</a>
+      </li>
+      <li><a href='index.php?id=log'>Журнал посещений</a>
+      </li>
+    </ul>
+    <!-- Навигация -->
+  </div>
+  <div id="footer">
+    <!-- Нижняя часть страницы -->
+    &copy; Супер-мега сайт, 2000 &ndash; <?= date('Y')?>
+      <!-- Нижняя часть страницы -->
+  </div>
+</body>
+
+</html>
